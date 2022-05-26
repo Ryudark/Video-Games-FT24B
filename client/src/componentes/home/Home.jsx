b4399babb82040840ff358636ce0c355a803c1da
Ryudark <steinu037@gmail.com>
2022-05-26 18:06:06 -0500

    Boton Neon

--- client/src/componentes/home/Home.jsx
+++ client/src/componentes/home/Home.jsx
@@ -7,7 +7,14 @@ export default function Home(){
         <div className="fondo3">
             <h1 className="title">HENRY VIDEOGAMES PI</h1>
             <Link to="/home">
-                <button className="boton">HOME</button>
+                <p className="boton">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Ingrese a la App
+                </p>
+                {/* <button className="boton">HOME</button> */}
             </Link>
         </div>
     )
